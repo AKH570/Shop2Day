210ae38b380bacea47e2b454aded26fdf6d57b44
Abir Hossain Khandokar <abir.h@mutualtrustbank.com>
2024-06-25 08:38:21 +0600

    update mailconfig to env

--- AKH_SHOP/settings.py
+++ AKH_SHOP/settings.py
@@ -160,10 +160,10 @@ EMAIL_USE_TLS=True
 DEFAULT_FROM_EMAIL=EMAIL_HOST_USER
 """
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'mail.abirhkhandokar.com'
-EMAIL_PORT = '465'
-EMAIL_HOST_USER = 'barakahclothstore@abirhkhandokar.com'
-EMAIL_HOST_PASSWORD = '9ZuQ*HayQGhm'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT',cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 DEFAULT_FROM_EMAIL=EMAIL_HOST_USER
