4d9a678ff28ea4b372353be092ffb3b39709021b
Abir Hossain Khandokar <abir.h@mutualtrustbank.com>
2024-06-27 08:09:08 +0600

    finishing2

--- Accounts/forms.py
+++ Accounts/forms.py
@@ -2,13 +2,13 @@ from django import forms
 from .models import User
 
 class RegistrationForm(forms.ModelForm):
-    first_name       = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','style':'border-color:mediumvioletred;border-radius:16px','placeholder':'First Name'})) 
-    last_name        = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','style':'border-color:mediumvioletred;border-radius:16px','placeholder':'Last Name'}))
-    username         = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','style':'border-color:mediumvioletred;border-radius:16px','placeholder':'Name'}))
-    email            = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control','style':'border-color:mediumvioletred;border-radius:16px','placeholder':'Email'}))
-    phone_no         = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','style':'border-color:mediumvioletred;border-radius:16px','placeholder':'Phone No'}))
-    password         = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','style':'border-color:mediumvioletred;border-radius:16px','placeholder':'Password'}))
-    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','style':'border-color:mediumvioletred;border-radius:16px','placeholder':'Confirm Password'}))
+    first_name       = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','style':'border-color:lightblue;border-radius:16px','placeholder':'First Name'})) 
+    last_name        = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','style':'border-color:lightblue;border-radius:16px','placeholder':'Last Name'}))
+    username         = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','style':'border-color:lightblue;border-radius:16px','placeholder':'Name'}))
+    email            = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control','style':'border-color:lightblue;border-radius:16px','placeholder':'Email'}))
+    phone_no         = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','style':'border-color:lightblue;border-radius:16px','placeholder':'Phone No'}))
+    password         = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','style':'border-color:lightblue;border-radius:16px','placeholder':'Password'}))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','style':'border-color:lightblue;border-radius:16px','placeholder':'Confirm Password'}))
     class Meta:
         model=User
         fields=['first_name','last_name','username','email','phone_no','password','checkbox']
